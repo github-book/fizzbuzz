ca9ebf6310b9a616b2dcae22e722487279815205
hirocaster <hohtsuka@gmail.com>
2013-03-18 17:40:05 +0900

    Fix fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,9 +2,9 @@ class Fizzbuzz
   def calculate number
     if number % 3 == 0 && number % 5 == 0
       'fizzbuzz'
-      elsif number % 3 == 0
+    elsif number % 3 == 0
       'fizz'
-      elsif number % 5 == 0
+    elsif number % 5 == 0
       'buzz'
     else
       number
