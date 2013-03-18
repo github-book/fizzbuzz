0e902c7710118e7c6c9432916851e23a78feac25
hirocaster <hohtsuka@gmail.com>
2013-03-18 17:10:58 +0900

    Fix fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,6 +3,8 @@ class Fizzbuzz
     if number % 3 == 0 && number % 5 == 0
       'fizzbuzz'
       elsif number % 3 == 0
+      'fizz'
+      elsif number % 5 == 0
       'buzz'
     else
       number
