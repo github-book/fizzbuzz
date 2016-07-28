ae5d12fccef8a44c5051ca935bc7c48828778e12
Shoichi Imamura <syoichiimamura@gmail.com>
2016-07-28 12:48:44 +0900

    Add Output GitHub

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,14 @@
+# 3, 5, 7
 class Fizzbuzz
-  def calculate number
+  def calculate(number)
     if number % 3 == 0 && number % 5 == 0
       'fizzbuzz'
     elsif number % 3 == 0
       'fizz'
     elsif number % 5 == 0
       'buzz'
+    elsif number.to_s.include? '7'
+      'GitHub'
     else
       number
     end
