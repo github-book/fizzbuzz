7b9768a2a497928a307d16737cf013bc5ba4dbdd
hmeguro <59855383+hmeguro@users.noreply.github.com>
2020-04-19 20:30:53 +0900

    Fix output GitHub

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,6 +2,8 @@ class Fizzbuzz
   def calculate number
     if number % 3 == 0 && number % 5 == 0
       'fizzbuzz'
+    elsif number % 2 == 0
+      'practice'
     elsif number % 3 == 0
       'fizz'
     elsif number % 5 == 0
