1c8c1094f127a07574e2f3f5a19f33d44a1eb14f
yushiro-div <61450307+yushiro-div@users.noreply.github.com>
2021-07-25 16:23:44 +0900

    Fix indent

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -6,8 +6,8 @@ class Fizzbuzz
       'fizz'
     elsif number % 5 == 0
       'buzz'
-      elsif number.to_s.include? '7'
-        'Github'
+    elsif number.to_s.include? '7'
+      'Github'
     else
       number
     end
