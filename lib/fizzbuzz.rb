26bf3cbb7e9fa19da9224ef53a4a448e9d4d6600
yushiro-div <61450307+yushiro-div@users.noreply.github.com>
2021-07-25 16:34:00 +0900

    fix githubtest

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,14 @@
 class Fizzbuzz
   def calculate number
     if number % 3 == 0 && number % 5 == 0
-      'fizzbuzz'
+      if number.to_s.include? '7'
+        'Github'
+      elsif number % 3 == 0 && number % 5 == 0
+        'FizzBazz'
     elsif number % 3 == 0
       'fizz'
     elsif number % 5 == 0
       'buzz'
-    elsif number.to_s.include? '7'
-      'Github'
     else
       number
     end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -20,4 +20,9 @@ describe Fizzbuzz do
     it { subject.calculate(15).should eq 'fizzbuzz' }
     it { subject.calculate(30).should eq 'fizzbuzz' }
   end
+  context 'Github number' do
+    it { subject.calculate(17).should eq 'github'}
+    it { subject.calculate(27).should eq 'github'}
+    it { subject.calculate(75).should eq 'github'}
+    it { subject.calculate(77).should eq 'github'}
 end
